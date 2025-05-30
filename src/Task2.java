7bfa0850b1af0ba1273b6bb206e64e85b394666d
HellenKopteva <elisia@yandex.ru>
2025-05-30 09:41:26 +0500

    Update Task2.java
    Edit message in Task2.

--- src/Task2.java
+++ src/Task2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Task2 {
     //TODO: Processing DateTimeParseException
     public static void main (String[] args){
-        System.out.println("Input data in format: YYYY-MM-DD, for example: 2022-01-31");
+        System.out.println("Input data in format: YYYY-MM-DD. For example: 2022-01-31");
         Scanner scanner = new Scanner(System.in);
         String dataS = scanner.nextLine();
         try {
